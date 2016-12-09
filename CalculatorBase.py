55dc1851df474bb8240f62d8736d21d0a3c33f1d
JZ6 <jz6jz6jz@gmail.com>
2016-12-09 16:52:37 -0500

    perf

--- CalculatorBase.py
+++ CalculatorBase.py
@@ -1,8 +1,17 @@
-def pen_or_pow(base: int, scaling: float, armor: int, cxp_power: int, cxp_pen: int):
-    assert cxp_pen + cxp_power == 60
-    dmg_reduction = 100/(100 + armor - 4*cxp_pen)
+def pen_or_pow(base_damage: int, scaling: float, armor: int, cxp_power: int, cxp_pen: int, armor_type: str):
+    pen_scale = 2
+    if armor_type[0].lower() == 'b':
+        pen_limit = armor/4;
+        pen_scale = 4
+    else:
+        pen_limit = armor/2
+
+    assert cxp_pen <= pen_limit
+    assert cxp_pen + cxp_power <= 60
+
+    dmg_reduction = 100/(100 + armor - pen_scale*cxp_pen)
     pow_increase = scaling*6*cxp_power
-    total_dmg = (base + pow_increase)*dmg_reduction
+    total_dmg = (base_damage + pow_increase)*dmg_reduction
     return total_dmg
 
-pen_or_pow(300, 1.0, 30, 60,0)
+#print(pen_or_pow(300, 1.0, 30, 60, 0, 'a'))
---
+++ Perf.py
@@ -0,0 +1,25 @@
+from CalculatorBase import pen_or_pow
+import matplotlib.pyplot as plt
+
+def pow_pen_balance(base_damage, scaling, armor, current_cxp, armor_type: str):
+    assert current_cxp <= 60
+    if armor_type[0].lower() == 'b':
+        pen_limit = armor/4;
+    else:
+        pen_limit = armor/2
+
+    max_damage = 0
+    max_pen = 0
+    max_pow = 0
+    for pen_cxp in range(0, int(pen_limit)+1):
+        pow_cxp = current_cxp - pen_cxp
+        current_dmg = pen_or_pow(base_damage, scaling, armor, pow_cxp, pen_cxp, armor_type)
+        if max_damage < current_dmg:
+            max_damage = current_dmg
+            max_pen = pen_cxp
+            max_pow = pow_cxp
+
+    return "Maximum damage for an attack with "+ str(base_damage) + " base damage and " + str(scaling) + " scaling at " + str(current_cxp) + " cxp is " + str(max_damage) + " from " + str(max_pen) + " cxp of pen and "+ str(max_pow) + " cxp of power on an enemy with " + str(armor) + " armor."
+
+
+print(pow_pen_balance(300, 1.0, 50, 60, 'a'))
